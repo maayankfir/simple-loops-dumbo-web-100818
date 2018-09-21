2fe67df4fdeca7bbe8922ee015a9c6e9f5e546a9
Maayan Kfir <mayankfir@gmail.com>
2018-09-21 21:45:24 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,8 +4,6 @@ def loop_iterator(number_of_times)
 
   end
 
-end
-
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
