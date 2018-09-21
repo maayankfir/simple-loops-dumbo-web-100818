480269a2ebfc77d2cb1b69b51283079bd5dbaf26
Maayan Kfir <mayankfir@gmail.com>
2018-09-21 21:59:05 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,9 +7,8 @@ counter = 0
     puts phrase
     counter += 1
     break if counter == number_of_times
-
   end
-  end
+end
 
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
